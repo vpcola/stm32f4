1967131d24e3fd4b516f61e7277d67efc7532cf5
vpcola <vpcola@gmail.com>
2016-01-29 07:06:20 +0800

    Removed pre-compiled dirs

--- EmbestNoEth/MDK-ARM/EmbestNoEth Configuration/utility.d
+++
@@ -1,4 +0,0 @@
-embestnoeth configuration\utility.o: ..\Src\utility.c
-embestnoeth configuration\utility.o: ../Inc/utility.h
-embestnoeth configuration\utility.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-embestnoeth configuration\utility.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\ctype.h
